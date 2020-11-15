59b6a7a850c80e12e52b23155ecfda71f410e4d7
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-11-15 06:47:00 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,20 +1,42 @@
 require_relative './part_1_solution.rb'
 
-def apply_coupons(cart, coupons)
-  coupons.each do |coupon|
-    name = coupon[:item]
-    if cart[name] && cart[name][:count] >= coupon[:num]
-      if cart["#{name} W/COUPON"]
-        cart["#{name} W/COUPON"][:count] += 1
+def apply_coupons(cart, coupon_array)
+  if cart.size == 0
+    return cart
+  elsif coupon_array.size == 0
+    return cart
+  else
+    consolidated_coupons_hash = {}
+    coupon_array.each do |coupon|
+      item_name = coupon[:item]
+      if consolidated_coupons_hash.key?(item_name) == false
+        coupon = coupon.merge({coupon_count: 1})
+        consolidated_coupons_hash[item_name] = coupon
       else
-        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
-        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+          consolidated_coupons_hash[item_name][:num] += coupon[:num]
+          consolidated_coupons_hash[item_name][:coupon_count] += 1
       end
-      cart[name][:count] -= coupon[:num]
+    end
+    consolidated_coupons_hash.each do |consolidated_key, value|
+      if cart.key?(consolidated_key)
+        consolidated_coupon_number = consolidated_coupons_hash[consolidated_key][:num]
+        cart_item_count = cart[consolidated_key][:count]
+        coupon_item_price = consolidated_coupons_hash[consolidated_key][:cost]
+        coupon_count = consolidated_coupons_hash[consolidated_key][:coupon_count]
+        cart_item_count_after_coupon = cart_item_count - consolidated_coupon_number
+        cart_item_clearance = cart[consolidated_key][:clearance]
+        
+        cart[consolidated_key][:count] = cart_item_count_after_coupon
+        cart["#{consolidated_key} W/COUPON"] = {price: coupon_item_price, clearance: cart_item_clearance, count: coupon_count}
+        #if cart[consolidated_key][:count] == 0
+          #cart.delete(consolidated_key)
+        #end
+      end
     end
   end
   cart
 end
+
 
 # def apply_coupons(cart, coupons)
 #   counter = 0
