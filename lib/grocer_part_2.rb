be565cdf479b2e635f2ef87764c447e404c7a1d3
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-11-15 06:10:41 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,6 +1,16 @@
 require_relative './part_1_solution.rb'
 
-
+def consolidate_cart(cart) 
+  new_cart = {} 
+  cart.each do |items_array| 
+    items_array.each do |item, attribute_hash| 
+      new_cart[item] ||= attribute_hash 
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+      new_cart[item][:count] = 1 
+  end 
+end 
+new_cart 
+end
 
 
 def apply_coupons(cart, coupons)
