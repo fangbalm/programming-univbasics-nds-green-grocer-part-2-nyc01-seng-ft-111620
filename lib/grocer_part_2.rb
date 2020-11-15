8cd8a8407b9b25f1424acefa939964dd447e39e5
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-11-15 06:24:30 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -8,23 +8,23 @@ def apply_coupons(cart, coupons)
     coupon_item_name = "#{coupons[counter][:item]} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
     if cart_item && cart_item[:count] >= coupons[counter][:num]
-    if cart_item_with_coupon
-      cart_item_with_coupon[:count] += coupons[counter][:num]
-      cart_item[:count] -= coupons[counter][:num]
-    else
-      cart_item_with_coupon = {
-        :item => coupon_item_name,
-        :price => coupons[counter][:cost] / coupons[:counter][num],
-        :count => coupons[counter][:num],
-        :clearance => cart_item[:clearance]
-      }
-      cart << cart_item_with_coupon
-      cart_item[:count] -= coupons[counter][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[counter][:num]
+        cart_item[:count] -= coupons[counter][:num]
+      else
+        cart_item_with_coupon = {
+          :item => coupon_item_name,
+          :price => coupons[counter][:cost] / coupons[:counter][num],
+          :count => coupons[counter][:num],
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[counter][:num]
       end
     end
-      counter += 1
+    counter += 1
   end
-  return car
+  return cart
 end
 
 
