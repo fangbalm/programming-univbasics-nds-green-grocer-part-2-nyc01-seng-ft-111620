0f2d7e78d0ff3ce9cfbb4a220ad3a0b1b351dbe5
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-11-15 06:35:12 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,32 +1,48 @@
 require_relative './part_1_solution.rb'
 
-
-def apply_coupons(cart, coupons)
-  counter = 0
-  while counter < coupons.length
-    cart_item = find_item_by_name_in_collection([coupons][counter][:item], cart)
-    coupon_item_name = "#{coupons[counter][:item]} W/COUPON"
-    cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
-    if cart_item && cart_item[:count] >= coupons[counter][:num]
-      if cart_item_with_coupon
-        cart_item_with_coupon[:count] += coupons[counter][:num]
-        cart_item[:count] -= coupons[counter][:num]
-      else
-        cart_item_with_coupon = {
-          :item => coupon_item_name,
-          :price => coupons[counter][:cost] / coupons[:counter][num],
-          :count => coupons[counter][:num],
-          :clearance => cart_item[:clearance]
-        }
-        cart << cart_item_with_coupon
-        cart_item[:count] -= coupons[counter][:num]
+new_cart = {}
+  cart.each do |grocery, info|
+    coupons.each do |coupon|
+      if grocery == coupon[:item] && info[:count] >= coupon[:num]
+        cart[grocery][:count] = cart[grocery][:count] - coupon[:num]
+        if new_cart[grocery + " W/COUPON"]
+          new_cart[grocery + " W/COUPON"][:count] += 1
+        else
+          new_cart[grocery + " W/COUPON"] = {:price => coupon[:cost], :clearance => cart[grocery][:clearance], :count => 1}
+        end
       end
     end
-    counter += 1
-  end
-  cart
-end
+    new_cart[grocery] = info
 
+  end
+  new_cart
+
+# def apply_coupons(cart, coupons)
+#   counter = 0
+#   while counter < coupons.length
+#     cart_item = find_item_by_name_in_collection([coupons][counter][:item], cart)
+#     coupon_item_name = "#{coupons[counter][:item]} W/COUPON"
+#     cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
+#     if cart_item && cart_item[:count] >= coupons[counter][:num]
+#       if cart_item_with_coupon
+#         cart_item_with_coupon[:count] += coupons[counter][:num]
+#         cart_item[:count] -= coupons[counter][:num]
+#       else
+#         cart_item_with_coupon = {
+#           :item => coupon_item_name,
+#           :price => coupons[counter][:cost] / coupons[:counter][num],
+#           :count => coupons[counter][:num],
+#           :clearance => cart_item[:clearance]
+#         }
+#         cart << cart_item_with_coupon
+#         cart_item[:count] -= coupons[counter][:num]
+#       end
+#     end
+#     counter += 1
+#   end
+#   cart
+# end
+# 
 
 
 def apply_clearance(cart)
